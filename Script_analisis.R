9bd003d1d7c868be015d533818020ffa3daad638
asael697 <asael_am@hotmail.com>
2023-08-17 10:32:24 -0300

    add more files

--- Script_analisis.R
+++ Script_analisis.R
@@ -2,6 +2,9 @@ library(bayesplot)
 library(cmdstanr)
 library(posterior)
 library(loo)
+library(xtable)
+library(ggplot2)
+library(cowplot)
 
 # ignora esta funcion
 dat_gen = function(N = 32,beta = rnorm(1),K = 4,alphaj = rnorm(K),seed  = NULL){
@@ -29,8 +32,7 @@ sm2 <- cmdstan_model("Stancodes/skew_normal.stan")
 d1 = list(n = length(LogGFN), J = 7, group = gl, y = LogGFN)
 
 # mcmc para modelo multinivel
-fit1 <- sm1$sample(data = d1, chains = 4, parallel_chains = 4, iter_sampling = 3000,
-                   refresh = 500)
+fit1 <- sm1$sample(data = d1, chains = 4, parallel_chains = 4, refresh = 500)
 
 # mcmc para modelo de Gomez
 fit2 <- sm2$sample(data = d1, chains = 4, parallel_chains = 4,refresh = 500)
@@ -43,8 +45,9 @@ colnames(fv1) = c(levels(glevels),'sigma')
 summarize_draws(fv1)
 
 # graficos de las posteriors
-mcmc_combo(fv1[,1:4])
-mcmc_combo(fv1[,5:8])
+g1 = mcmc_combo(fv1[,1:4],gg_theme = theme(legend.position = "none"))
+g2 = mcmc_combo(fv1[,5:8])
+cowplot::plot_grid(g1,g2,ncol = 2,rel_widths = c(1, 1.2))
 
 # Leave one out modelo multinivel
 ll1 = fit1$draws(variables = "log_lik",format = "matrix")
@@ -55,4 +58,4 @@ ll2 = fit2$draws(variables = "log_lik",format = "matrix")
 loo2 = loo(ll2)
 
 comp = loo_compare(loo1,loo2)
-print(comp,simplify = FALSE, digits = 3)
+xtable(print(comp,simplify = FALSE, digits = 2))
